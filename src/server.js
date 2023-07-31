e32f298c9025dc4d573e80e3e6c8815b0471b161
minji <321963741@naver.com>
2023-07-31 11:08:22 +0900

    feat: mysql docker-compose로 셋팅하여 db랑 server랑 연결

---
+++ src/server.js
@@ -0,0 +1,44 @@
+// import packages
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import xss from "xss-clean";
+import helmet from "helmet";
+import db from "./db/db.js";
+
+// app set
+const PORT = process.env.PORT ?? 5000;
+const app = express();
+
+// extra packages middleware setup
+app.set("trust proxy", 1);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(cors());
+app.use(cookieParser());
+
+//  routers
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome minji" });
+});
+
+// start server
+const startServer = async () => {
+  db.connect((err) => {
+    if (err) {
+      console.error("❌ Error connecting to the database:", err.message);
+      return;
+    }
+
+    console.log("✅ Success connection with db");
+    app.listen(PORT, () => {
+      console.log(`✅ Listening for server on port ${PORT}`);
+    });
+  });
+};
+
+startServer();
