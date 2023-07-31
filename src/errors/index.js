e9a93531ba104c7957702b61fc69520d7bb32185
minji <321963741@naver.com>
2023-07-31 11:42:05 +0900

    feat: error handling architecture 구성

---
+++ src/errors/index.js
@@ -0,0 +1,13 @@
+import CustomAPIError from "./custom-api.js";
+import UnauthenticatedError from "./unauthenticated.js";
+import NotFoundError from "./not-found.js";
+import BadRequestError from "./bad-request.js";
+import UnauthorizedError from "./unAuthorized.js";
+
+export {
+  CustomAPIError,
+  UnauthenticatedError,
+  UnauthorizedError,
+  NotFoundError,
+  BadRequestError,
+};
