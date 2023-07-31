30b6431f977700d2dd4f505ae24bd3b492394dcd
minji <321963741@naver.com>
2023-07-31 17:25:22 +0900

    refactor: jwt expired 시간 정해주기

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -3,7 +3,9 @@ import jwt from "jsonwebtoken";
 
 //  1. create JWT
 const createJWT = (payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
   return token;
 };
 
