75cfef309b6eb4954882a7dfd07644cab32051d2
minji <321963741@naver.com>
2023-08-02 10:18:46 +0900

    chore: db logic 수정으로인한 package 정리

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,4 @@
-import mysql from "mysql2";
-import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-dotenv.config();
-
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
 
 const primsaDB = new PrismaClient();
 export default primsaDB;
