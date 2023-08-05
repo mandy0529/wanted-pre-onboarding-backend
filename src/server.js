e358976458fa1bf15b5409f19deeddac3144f0cf
minji <321963741@naver.com>
2023-08-05 18:00:54 +0900

    refactor: dotenv import 추가

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import "express-async-errors";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import xss from "xss-clean";
-import helmet from "helmet";
-import primsaDB from "./db/db.js";
+import primsaDB from "../db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // import files
 import {
@@ -27,14 +28,13 @@ const base_url = "/api/v1";
 app.set("trust proxy", 1);
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(helmet());
-app.use(xss());
 app.use(cors());
 app.use(cookieParser());
-
 //  routers
 app.use(`${base_url}/user`, userRouter);
 app.use(`${base_url}/post`, postRouter);
+
+app.use("/", express.static("public"));
 
 // 🔥 error middleware 항상 마지막 미들웨어에 위치 🔥
 app.use(notFoundMiddleware);
@@ -44,12 +44,12 @@ app.use(errorHandlerMiddleware);
 const startServer = async () => {
   try {
     await primsaDB.$connect();
-    console.log("Prisma DB가 연결되었습니다.");
+    console.log("✅ Prisma DB가 연결되었습니다.");
     app.listen(PORT, () => {
       console.log(`✅ Listening for server on port ${PORT}`);
     });
   } catch (error) {
-    console.error("Prisma DB 연결에 실패했습니다.", error);
+    console.error("❌ Prisma DB 연결에 실패했습니다.", error);
   }
 };
 
