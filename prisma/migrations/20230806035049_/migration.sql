e6f084e3b2eb9ec06036e7a82cf592c482e43d58
minji <321963741@naver.com>
2023-08-06 13:01:08 +0900

    feat: migrations file git 추가

---
+++ prisma/migrations/20230806035049_/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(255) NOT NULL DEFAULT '',
+    `password` VARCHAR(255) NOT NULL DEFAULT '',
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL DEFAULT '',
+    `content` VARCHAR(1000) NOT NULL DEFAULT '',
+    `authorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `post` ADD CONSTRAINT `post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
